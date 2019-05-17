bf31bed4075898bb9eb4cafa0b8ecdf3b42ca483
余亮 <wudy.yu@huolala.cn>
2019-05-17 16:44:41 +0800

    处理swoole与tp框架的关系

--- server/swoole_http_server.php
+++ server/swoole_http_server.php
@@ -16,13 +16,81 @@ $http = new swoole_http_server('127.0.0.1', 9501);
 //开启swoole_http_server :  php  swoole_http_server.php
 //在浏览器访问 ： http://127.0.0.1:9501/index.html
 $http->set([
+    //设置了worker_num和task_worker_num超过1时，每个进程都会触发一次onWorkerStart事件，可通过判断$worker_id区分不同的工作进程
+    'work_num' => 5,
+    'task_worker_num' => 10,
+
     'enable_static_handler' => true,
     'document_root' => '/Users/yuliang/swooleTp/public/static',
 ]);
 
 $http->on('request', function (swoole_http_request $request, swoole_http_response $response){
+    //将swoole的请求获取参数转换为PHP原生的形式
+    if(isset($request->post))
+    {
+       foreach ($request->post as $k=>$v)
+       {
+           $_POST[$k] = $v;
+       }
+    }
+
+    //PHP中超全局不会被释放(会导致每次的请求参数都累计在一个数组里面)，需要我们手动去释放掉
+    if(!empty($_GET))
+    {
+        unset($_GET);
+    }
+    if(isset($request->get))
+    {
+        foreach ($request->get as $k=>$v)
+        {
+            $_GET[$k] = $v;
+        }
+    }
+
+    if(isset($request->header))
+    {
+        foreach ($request->header as $k=>$v)
+        {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+
+    if(isset($request->server))
+    {
+        foreach ($request->server as $k=>$v)
+        {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+
+    ob_start();
+    //执行应用
+    try {
+        think\App::run()->send();
+    }catch (\Exception $e){
+        echo '异常错误' . $e->getCode() . $e->getMessage();
+        exit;
+    }
+    $res = ob_get_contents();
+    ob_end_clean();
+
     $response->cookie('sigma', 'xssss', time() + 1800);
-    $response->end("wudy.yu".json_decode($request->get));
+    //end操作后将向客户端浏览器发送HTML内容,只能调用一次
+    $response->end($res);
+});
+
+/**
+ * 事件在Worker进程/Task进程启动时发生
+ * @param $worker_id : worker进程的id
+ */
+$http->on('WorkerStart', function(swoole_http_server $serv , $worker_id){
+    // 定义应用目录
+    define('APP_PATH', __DIR__ . '/../application/');
+    require_once  __DIR__ .   '/../thinkphp/base.php';;
+});
+
+$http->on('Task', function (swoole_http_server $serv, swoole_server_task $task){
+
 });
 
 $http->start();
